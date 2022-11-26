b63e492328390d5abc0f4dcce21db36d1dc48712
RAHMA_BILAL <rahma.bilal15@gmail.com>
2022-11-26 15:53:33 +0000

    adding questions for the readme file

--- index.js
+++ index.js
@@ -1,65 +1,61 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
-const generateMarkdown = require("./utils/generateMarkdown")
+const generateMarkdown = require("./utils/generateMarkdown");
 
 const questions = () => {
-return inquirer.prompt([
+  return inquirer.prompt([
     {
-        type: "input",
-        message: "projectTitle",
-        name: "What is the title of your project?"
+      type: "input",
+      message: "projectTitle",
+      name: "What is the title of your project?",
     },
     {
-        type: "input",
-        message: "description",
-        name: "What is the description of this README?"
+      type: "input",
+      message: "description",
+      name: "What is the description of this README?",
     },
     {
-        type: "input",
-        message: "installation",
-        name: "How do you install this project?"
+      type: "input",
+      message: "installation",
+      name: "How do you install this project?",
     },
     {
-        type: "input",
-        message: "usage",
-        name: "How will you use this application?"
+      type: "input",
+      message: "usage",
+      name: "How will you use this application?",
     },
     {
-        type: "input",
-        message: "summary",
-        name: "Provide a project summary"
+      type: "input",
+      message: "summary",
+      name: "Provide a project summary",
     },
     {
-        type: "input",
-        message: "functionality",
-        name: "What function does this project perform?"
+      type: "input",
+      message: "functionality",
+      name: "What function does this project perform?",
     },
     {
-        type: "list",
-        message: "license",
-        name: "How do you want to license this project?",
-        choices: [
-            "MIT", "ISC", "Mozilla", "IBM"
-        ],
+      type: "list",
+      message: "license",
+      name: "How do you want to license this project?",
+      choices: ["MIT", "ISC", "Mozilla", "IBM"],
     },
     {
-        type: "input",
-        message: "testFunction",
-        name: "Is there a test function for the application?"
+      type: "input",
+      message: "testFunction",
+      name: "Is there a test function for the application?",
     },
     {
-        type: "input",
-        message: "github",
-        name: "What is your GitHub username?"
+      type: "input",
+      message: "github",
+      name: "What is your GitHub username?",
     },
     {
-        type: "input",
-        message: "email",
-        name: "What is your email address?"
-    }
-
-    ]
-)};
-
+      type: "input",
+      message: "email",
+      name: "What is your email address?",
+    },
+  ]);
+};
 
