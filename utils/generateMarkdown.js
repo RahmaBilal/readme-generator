cdd1fbd0ee1bc6ee0e232e9214e8de4c45f5cf46
RAHMA_BILAL <rahma.bilal15@gmail.com>
2022-11-29 20:39:03 +0000

    final touches

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,46 +1,111 @@
-function generateMarkdown(data) {
-    return `# ${data.title}
-    ## Badge
-    
-    ![Badge](http://img.shields.io/badge/license-${data.license}-blue.png)
-    <br>
-    
-    ## Table of Contents
-    * [License](#license)
-    * [Description](#description)
-    * [Installation](#installation)
-    * [Usage](#usage)
-    * [Contributing](#contributing)
-    * [Tests](#tests)
-    * [Questions](#questions)
-    
-    ## License
-    
-    The application is authorized for usage in accordance with the ${data.license} license.
-    
-    Link: [${data.license}] https://opensource.org/licenses/${data.license}
-    
-    
-    ## Description
-    ${data.description}
-    
-    ## Installation
-    ${data.install}
-    
-    ## Usage
-    ${data.usage}
-    
-    ## Contributing  
-    ${data.contributing}
-    
-    ## Tests
-    ${data.testing}
-    
-    ## Questions
-    ### If you have any questions, contact me at: 
-    Github: [${data.github}](https://github.com/${data.github})
-    <br>
-    Email: ${data.email}`;
+//  A function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+const renderLicenseBadge = (license) => {
+  let licenseBadge;
+  switch (license) {
+    case "MIT":
+      licenseBadge =
+        "https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT";
+      break;
+    case "ISC":
+      licenseBadge =
+        "https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC";
+      break;
+    case "MOZILLA":
+      licenseBadge =
+        "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+      break;
+    case "IBM":
+      licenseBadge =
+        "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
+      break;
+    case "None":
+      licenseBadge = "";
+      break;
   }
+};
+// Create a function that returns the license link
+// If there is no license, return an empty string
+const renderLicenseLink = (license) => {
+  let licenseLink;
+  switch (license) {
+    case "MIT":
+      licenseLink = "\nFind out more on: https://opensource.org/licenses/MIT";
+      break;
+    case "ISC":
+      licenseLink = "\nFind out more on: https://opensource.org/licenses/ISC";
+      break;
+    case "MOZILLA":
+      licenseLink =
+        "\nFind out more on: https://opensource.org/licenses/MOZILLA";
+      break;
+    case "IBM license":
+      licenseLink =
+        "\nFind out more on: https://opensource.org/licenses/IPL-1.0";
+      break;
+    case "None":
+      licenseLink = "";
+      break;
+  }
+  return licenseLink;
+};
+
+//  A function that returns the license section of README
+// If there is no license, return an empty string
+const renderLicenseSection = (license) => {
+  if (license === "None") {
+    return "No";
+  } else {
+    return license;
+  }
+};
+
+//  A function to generate markdown for README
+const generateMarkdown = (data) => {
+  console.log(data);
+  return `# ${data.title}
+    
+  ![Badge](http://img.shields.io/badge/license-${data.license}-blue.png)
+    
   
-  module.exports = generateMarkdown;
+  ## Table of Contents
+  
+  * [Description](#description)
+  * [Usage](#usage)
+  * [Installation](#installation)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+    
+    
+  ## Description:
+    ${data.description}
+  
+  ## Usage:
+    ${data.usage}
+  
+  ## Installation
+    ${data.installation}
+    
+  
+  ## License: 
+    ${data.license}
+  
+   ${renderLicenseLink(data.license)}
+
+  ## Contributing: 
+    ${data.contributing}
+  
+  ## Tests:
+    ${data.tests}
+
+  ## Questions:
+    If you have any queries about this project or you would like to contribute, then please contact me on ${
+      data.email
+    }.
+    This repo can be viewed at https://github.com/${data.github}
+  `;
+};
+
+module.exports = generateMarkdown;
